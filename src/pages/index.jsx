229674fc99ba8011e7e444234a629069e99b3069
Olivier Dijkstra <olivierdijkstra@gmail.com>
2021-03-13 19:14:38 +0100

    :construction:  removed bootstrapping files

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,7 @@
-import { Button, Typography } from '@iseats/apollo';
-import Layout from 'components/Layout/Layout';
-
-function Page() {
-    const { Title, Text, Paragraph } = Typography;
-
-    return (
-        <Layout>
-            <Title inverse>Introduction.</Title>
-
-            <Text inverse>Hello world.</Text>
-        </Layout>
-    );
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello World.</h1>
+    </div>
+  );
 }
-
-export default Page;
