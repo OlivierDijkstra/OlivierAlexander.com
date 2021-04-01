a4f6156cee760120aaf7f00e4f8dbf1f353fc76a
Olivier Dijkstra <olivierdijkstra@gmail.com>
2021-04-01 16:50:53 +0200

    :fire: (Removed aurora effect)

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,34 +1,24 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import Head from 'next/head';
 import 'styles/global.css';
-import { AuroraContext } from 'context/AuroraContext';
 
 function MyApp({ Component, pageProps }) {
-	const [aurora, setAurora] = useState({ x: 0, y: 0 });
-
-	function handleMouseMove(e) {
-		const size = document.body.getBoundingClientRect();
-
-		setAurora({
-			x: e.clientX - (size.left + size.width / 2),
-			y: e.clientY - (size.top + size.height / 2)
-		});
-	}
-
 	return (
-		<div onMouseMove={handleMouseMove} className="content">
-			<title>Olivier Alexander</title>
+		<>
+			<Head>
+				<title>Olivier Alexander</title>
 
-			<AuroraContext.Provider value={{ aurora }}>
-				<Component {...pageProps} />
-			</AuroraContext.Provider>
-		</div>
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+
+				<link
+					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+
+			<Component {...pageProps} />
+		</>
 	);
 }
-
-MyApp.propTypes = {
-	Component: PropTypes.any.isRequired,
-	pageProps: PropTypes.any.isRequired
-};
 
 export default MyApp;
